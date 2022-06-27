f421f0cd18adab9c53d54b099b5b3987d8cd6bcb
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-27 19:11:20 +0530

    Time: 1 ms (63.08%), Space: 41.8 MB (60.28%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,15 +1,14 @@
 class Solution {
     public int strStr(String haystack, String needle) {
         // check for the substring of that particular length of needle in the haystack
-      int l1 = haystack.length(), l2 = needle.length();
-        if (l1 < l2) {
+        int l1 = haystack.length();
+        int l2 = needle.length();
+        if(l1 < l2)
             return -1;
-        } else if (l2 == 0) {
+        else if(l2 == 0)
             return 0;
-        }
-        int threshold = l1 - l2;
-        for (int i = 0; i <= threshold; ++i) {
-            if (haystack.substring(i,i+l2).equals(needle)) {
+        for(int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if(haystack.substring(i, i+l2).equals(needle)) {
                 return i;
             }
         }
